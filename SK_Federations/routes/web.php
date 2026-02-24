3b6d5d66377b053f2b23e61034fc26adaf5aee7e
jeromebalberona412-sudo <jeromebalberona412@gmail.com>
2026-02-24 14:08:03 +0800

    skfed
    skfed

--- SK_Federations/routes/web.php
+++ SK_Federations/routes/web.php
@@ -3,5 +3,30 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
+
+Route::get('/login', function () {
+    return view('auth.login');
+})->name('login');
+
+Route::post('/login', function () {
+
+    return redirect('/dashboard');
+})->name('login.submit');
+
+Route::get('/password/request', function () {
+    return view('auth.forgot-password');
+})->name('password.request');
+
+Route::post('/password/email', function () {
+    return back()->with('status', 'Password reset link sent!');
+})->name('password.email');
+
+Route::get('/dashboard', function () {
+    return 'Welcome to Dashboard!';
+})->name('dashboard');
+
+Route::post('/logout', function () {
+    return redirect('/login');
+})->name('logout');
