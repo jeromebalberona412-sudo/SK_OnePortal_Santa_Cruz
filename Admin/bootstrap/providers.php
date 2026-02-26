96822a6c4ed9954edd998e4077f310d705193555
JEZCS <belangoyfrankien@gmail.com>
2026-02-26 23:52:22 +0800

    add lng ng gitignore

--- Admin/bootstrap/providers.php
+++ Admin/bootstrap/providers.php
@@ -1,12 +1,12 @@
 <?php
 
 return [
-    App\Providers\AppServiceProvider::class,
-    Laravel\Fortify\FortifyServiceProvider::class,
-    App\Providers\FortifyServiceProvider::class,
-    App\Modules\Shared\Providers\SharedServiceProvider::class,
-    App\Modules\Authentication\Providers\AuthenticationServiceProvider::class,
-    App\Modules\AuditLog\Providers\AuditLogServiceProvider::class,
-    App\Modules\Dashboard\Providers\DashboardServiceProvider::class,
     App\Modules\Add_Account\Providers\AddAccountServiceProvider::class,
+    App\Modules\AuditLog\Providers\AuditLogServiceProvider::class,
+    App\Modules\Authentication\Providers\AuthenticationServiceProvider::class,
+    App\Modules\Dashboard\Providers\DashboardServiceProvider::class,
+    App\Modules\Shared\Providers\SharedServiceProvider::class,
+    App\Providers\AppServiceProvider::class,
+    App\Providers\FortifyServiceProvider::class,
+    Laravel\Fortify\FortifyServiceProvider::class,
 ];
